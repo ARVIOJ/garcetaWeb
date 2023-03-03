6c5a815e8481659387548211fc915f38bf8f4500
ARVIOJ <ol439950@uaeh.edu.mx>
2023-03-03 11:54:57 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,99 @@
+<?php
+include 'templates/head.html';
+include 'templates/navbar.html';
+include 'templates/header.html';
+include 'templates/nosotros.html';
+?>
+
+<br><br id="Slogan"><br>
+    <!-- Head -->
+    <!-- Muebles a presentar Medidas para este diseño: 731 x 416-->
+    <div class="container px-4 px-lg-5 my-5" id="AboutUs">
+        <!-- Imagen principal -->
+        <div class="row gx-0 mb-4 mb-lg-5 align-items-center bg-light">
+            <div class="col-xl-6 col-lg-5"><img class="img-fluid mb-3 mb-lg-0 rounded" src="assets/img/Mueble01BN.png" alt="Recamara" /></div>
+            <div class="col-xl-6 col-lg-5">
+                <div class="featured-text text-center text-lg-left">
+                    <h4>Muebles de calidad</h4>
+                    <p class="text-black-50 mb-0 px-5">Mejor que comprar solo Muebles
+                        En Muebles Garza nos aseguramos no solo de la imagen si no de la calidad
+                        del producto,asegurandonos que siempre se ofrezca lo mejor al cliente</p>
+                </div>
+            </div>
+        </div>
+
+    <section class="projects-section bg-white" id="Muebles">
+        <!-- Producto Uno -->
+        <div class="mix-black-20 py-3 mx-3 rounded my-3">
+            <div class="row gx-0 mb-5 mb-lg-0 justify-content-center bg-light py-3">
+                <div class="col-lg-6">
+                    <!-- Imagen -->
+                    <img class="img-fluid rounded" src="assets/img/Mueble02mod.png" alt="..." />
+                </div>
+                <div class="col-lg-5">
+                    <div class="bg-white text-center h-100 project rounded">
+                        <div class="d-flex h-100">
+                            <div class="project-text w-100 my-auto text-center text-lg-left">
+                                <h4 class="text-black">Nombre_Producto</h4>
+                                <p class="mb-0 text-black-50">Description about the product</p><br>
+                                <p class="mb-0 text-black-50">Price </p><code>"Precio del producto"</code><br><br><br>
+                                <button class="btn btn-warning">Añadir al carrito</button>
+                                <!--hr class="d-none d-lg-block mb-0 ms-0" /-->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Producto 2 -->
+        <div class="mix-black-20 py-3 mx-3 rounded my-3">
+            <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
+                <div class="col-lg-6">
+                    <!-- Imagen -->
+                    <img class="img-fluid rounded" src="assets/img/Mueble03.png" alt="..." />
+                </div>
+                <div class="col-lg-5">
+                    <div class="bg-white text-center h-100 project rounded">
+                        <div class="d-flex h-100">
+                            <div class="project-text w-100 my-auto text-center text-lg-left">
+                                <h4 class="text-black">Nombre_Producto</h4><br>
+                                <p class="mb-0 text-black-50">Description about the product</p><br><br>
+                                <p class="mb-0 text-black-50">Price </p><code>"Precio del producto"</code><br><br><br>
+                                <button class="btn btn-warning">Añadir al carrito</button>
+                                <!--hr class="d-none d-lg-block mb-0 ms-0" /-->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- . . . -->
+        <!-- Producto n-->
+        <div class="mix-black-20 py-3 mx-3 rounded my-3">
+            <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
+                <div class="col-lg-6">
+                    <!-- Imagen -->
+                    <img class="img-fluid rounded" src="assets/img/Mueble04mod.png" alt="..." />
+                </div>
+                <div class="col-lg-5">
+                    <div class="bg-white text-center h-100 project rounded">
+                        <div class="d-flex h-100">
+                            <div class="project-text w-100 my-auto text-center text-lg-left">
+                                <h4 class="text-black">Nombre_Producto</h4><br>
+                                <p class="mb-0 text-black-50">Description about the product</p><br><br>
+                                <p class="mb-0 text-black-50">Price </p><code>"Precio del producto"</code><br><br><br>
+                                <button class="btn btn-warning">Añadir al carrito</button>
+                                <!--hr class="d-none d-lg-block mb-0 ms-0" /-->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </section>
+
+<?php
+include 'templates/ubicycontacto.html';
+include 'templates/footer.html';
+?>
